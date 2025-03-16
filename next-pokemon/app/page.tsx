b9aac8b2c0c546643fc558c7e9e45e9dfd574cb5
angelgigi <wangqi1994>
2025-03-16 23:19:08 +0800

    提交
    提交

---
+++ next-pokemon/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col ">
+      <p>欢迎来到宝可梦世界</p>
+      <p>total{}</p>
+      <Image src="" alt="" />
+    </div>
+  );
+}
+
